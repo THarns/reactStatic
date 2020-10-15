cd5144358a470735999480b46440d300997b8d80
THarns <tharnsbe@gmail.com>
2020-10-15 00:23:50 -0400

    Fixed CSS overflow issue.

--- src/js/pages/contact.js
+++ src/js/pages/contact.js
@@ -2,7 +2,7 @@ import React from 'react';
 import Footer from '../components/Footer/footer.js';
 import { Transition, animated } from 'react-spring/renderprops'
 
-import '../../scss/home.scss';
+import '../../scss/contact.scss';
 import '../../scss/index.scss';
 
 const Contact = () => {
